aa35796d3b61031b86ea0f35798a83e741963648
bboyho <ho.chan@colorado.edu>
2016-07-05 19:18:21 -0600

    Update Atmega32U4Serial_Passthrough.ino

--- Atmega32U4Serial_Passthrough.ino
+++ Atmega32U4Serial_Passthrough.ino
@@ -1,15 +1,25 @@
 /*Atmega32U4Serial_Passthrough.ino
- by Ho Yun "Bobby" Chan
+ Modified by: Ho Yun "Bobby" Chan
+ By: Jim Lindblom
  @ SparkFun Electronics Inc.
+ on July 5th, 2016
+ 
+ license: Public Domain - please use this code however you'd like.
+ It's provided as a learning tool.
  
  This example code passes serial data between an Arduino's serial monitor
  and a device connected to an Atmega32U4's hardware UART (pin 0 and 1).
  Characters sent through the serial monitor will display it back on the
- serial monitor.  This code was designed for an XBee connected to the
- FioV3's XBee sockets. The design of the FioV3's XBee sockets use the 
- hardware UART pins. The Atmega32U4 Serial Passthrough will work for other
- Arduinos with an Atmega32U4 such as the Pro Micro, Arduino Leonardo, and
- LilyPad Arduino USB.*/
+ serial monitor. This code was designed to send serial data from XBees 
+ attached to FioV3's XBee sockets. The design of the FioV3's XBee sockets  
+ use the hardware UART pins. The Atmega32U4 Serial Passthrough will work
+ for other Arduinos with an Atmega32U4 such as the Pro Micro, Arduino 
+ Leonardo, and LilyPad Arduino USB. 
+ 
+ For assistance on configuring two XBees, check out Digi's Tutorials:
+ XBee Series 1: http://examples.digi.com/get-started/basic-xbee-802-15-4-chat/
+ XBee Series 2: http://examples.digi.com/get-started/basic-xbee-zb-zigbee-chat/
+ */
 
 char val; //init global var for serial characters being sent from XBee
 
